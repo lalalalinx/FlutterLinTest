8586478072f823c64c9327f3b5b9b6d170d5e32c
lalalalinx <kullanart.01@gmail.com>
2021-10-20 21:08:28 +0700

    แก้ของกูเอง

--- lib/Login_Register/register.dart
+++ lib/Login_Register/register.dart
@@ -11,11 +11,29 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final formkey = GlobalKey<FormState>();
 
+  Future save() async {
+    var res = await http.post(
+        Uri.parse('http://10.0.2.2:3000/login-register/register'),
+        headers: <String, String>{
+          'Context-Type': 'application/json;charSet=UTF-8'
+        },
+        body: <String, String>{
+          'userName': usernameController.text,
+          'email': emailController.text,
+          'password': passwordController.text,
+          'employeeID': employeeidController.text
+        });
+    print(res.body);
+  }
+
+  //var
 
   //controller
+  final usernameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
+  final employeeidController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final shape = RoundedRectangleBorder(
@@ -29,41 +47,66 @@ class _RegisterState extends State<Register> {
           padding: const EdgeInsets.all(40),
           child: Form(
             key: formkey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email Address',
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      hintText: 'Username',
+                    ),
+                    
+                    controller: usernameController,
                   ),
-                  controller: emailController,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email Address',
+                      hintText: 'Email Address',
+                    ),
+                    controller: emailController,
+                    
                   ),
-                  controller: passwordController,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Confirm Password',
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Password',
+                    ),
+                    controller: passwordController,
                   ),
-                  controller: confirmPasswordController,
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.deepPurple[700],
-                    primary: Colors.white,
-                    padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
-                    shape: shape,
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      hintText: 'Confirm Password',
+                    ),
+                    controller: confirmPasswordController,
                   ),
-                  child: Text("Confirm"),
-                  onPressed: () {
-                    print(emailController.text);
-                    print(passwordController.text);
-                    print(confirmPasswordController.text);
-                  },
-                )
-              ],
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'EmployeeID',
+                      hintText: 'EmployeeID',
+                    ),
+                    controller: employeeidController,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.deepPurple[700],
+                      primary: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      shape: shape,
+                    ),
+                    child: Text("Confirm"),
+                    onPressed: () {
+                      // save();
+                      print(usernameController.text);
+                      print(emailController.text);
+                      print(passwordController.text);
+                      print(confirmPasswordController.text);
+                      print(employeeidController.text);
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ));
