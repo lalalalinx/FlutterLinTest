a64dcd210df3525310fe266fbcebda63662d3cfb
lalalalinx <kullanart.01@gmail.com>
2021-10-21 13:55:02 +0700

    register แก้รอบ 2

--- lib/Login_Register/register.dart
+++ lib/Login_Register/register.dart
@@ -42,79 +42,111 @@ class _RegisterState extends State<Register> {
       borderRadius: BorderRadius.circular(60),
     );
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text("Register"),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back_ios,
+                size: 25,
+                color: Colors.black,
+              )),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(40),
+          padding: const EdgeInsets.symmetric(vertical: 0,horizontal: 40),
           child: Form(
             key: formkey,
             child: SingleChildScrollView(
               child: Column(
                 children: [
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   SizedBox(
-                    height:20.0,
+                    height: 10,
+                  ),
+                  Text(
+                    "Create an Account",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
                   ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Username',
                       hintText: 'Username',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                      contentPadding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     ),
                     controller: usernameController,
                   ),
                   SizedBox(
-                height: 30.0,
-              ),
+                    height: 20.0,
+                  ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Email Address',
                       hintText: 'Email Address',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
-                      
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     ),
                     controller: emailController,
                   ),
                   SizedBox(
-                height: 30.0,
-              ),
+                    height: 20.0,
+                  ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Password',
                       hintText: 'Password',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     ),
                     controller: passwordController,
                   ),
                   SizedBox(
-                height: 30.0,
-              ),
+                    height: 20.0,
+                  ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'Confirm Password',
                       hintText: 'Confirm Password',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     ),
                     controller: confirmPasswordController,
                   ),
                   SizedBox(
-                height: 30.0,
-              ),
+                    height: 20.0,
+                  ),
                   TextFormField(
                     decoration: InputDecoration(
                       labelText: 'EmployeeID',
                       hintText: 'EmployeeID',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(6.0))),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+                          borderRadius: BorderRadius.all(Radius.circular(6.0))),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
                     ),
                     controller: employeeidController,
                   ),
@@ -137,7 +169,12 @@ class _RegisterState extends State<Register> {
                         print(confirmPasswordController.text);
                         print(employeeidController.text);
                       },
-                      child: const Text('Confirm', style: TextStyle(fontSize: 18,),),
+                      child: const Text(
+                        'Confirm',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
                       style: ElevatedButton.styleFrom(
                         primary: Colors.deepPurple[700],
                         fixedSize: const Size(320, 55),
