c5338e973dbf5f5d0ca80536eb334b96d0da9950
lalalalinx <kullanart.01@gmail.com>
2021-10-21 14:55:25 +0700

    Profile บักห่า

--- lib/Screens/ProfileView.dart
+++ lib/Screens/ProfileView.dart
@@ -1,27 +1,30 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'Profile_build.dart';
 
 class ProfileView extends StatefulWidget {
-  const ProfileView({ Key? key }) : super(key: key);
+  const ProfileView({Key? key}) : super(key: key);
 
   @override
   _ProfileViewState createState() => _ProfileViewState();
 }
 
 class _ProfileViewState extends State<ProfileView> {
-  List<String> quotes = [
-    '11111111111',
-    '22222',
-    '33'
-  ];
-  
+  List<String> quotes = ['11111111111', '22222', '33'];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.grey[600], 
+        color: Colors.white,
+      ),
+      child: Scaffold(
+        appBar: ProfileBuild(context),
+        body: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [],
+        ),
       ),
     );
   }
