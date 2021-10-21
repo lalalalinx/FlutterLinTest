c5833e9e9e36c2c597be747165631a90bc568683
lalalalinx <kullanart.01@gmail.com>
2021-10-21 18:16:15 +0700

    ทำ Profile

---
+++ lib/Profile/ProfileData.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: file_names, non_constant_identifier_names
+
+class ProfileData{
+  final String accountName;
+  final String image;
+  final String name;
+  final String surname;
+  final String email;
+  final String tel;
+  final String zIP;
+  final String city; 
+  final String street; 
+  
+
+const ProfileData({
+  required this.accountName,
+  required this.image,
+  required this.name,
+  required this.surname,
+  required this.email,
+  required this.tel,
+  required this.zIP,
+  required this.city,
+  required this.street,
+  });
+}
--- lib/Screens/ProfileView.dart
+++ lib/Screens/ProfileView.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: file_names, prefer_const_constructors
 
+import 'package:chatki_project/Profile/ProfileData.dart';
+import 'package:chatki_project/Profile/ProfileUserData.dart';
 import 'package:flutter/material.dart';
-import 'Profile_build.dart';
+import 'package:chatki_project/Profile/ProfildWidget.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({Key? key}) : super(key: key);
@@ -15,17 +17,99 @@ class _ProfileViewState extends State<ProfileView> {
 
   @override
   Widget build(BuildContext context) {
+    final user = ProfileUserData.myUser;
+
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
       ),
       child: Scaffold(
-        appBar: ProfileBuild(context),
+        floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
         body: ListView(
           physics: BouncingScrollPhysics(),
-          children: [],
+          children: [
+            ProfileWidget(
+              image: user.image,
+              onClicked: () async {},
+            ),
+            const SizedBox(height: 24),
+            buildName(user),
+          ],
         ),
       ),
     );
   }
+
+  Widget buildName(user) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                user.accountName,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Text(
+              'Name - Surname',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              user.name+'  '+ user.surname,
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Email',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              user.email,
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Tel',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              user.tel,
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'ZIP - City - Street',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              user.zIP + ' - ' + user.city + ' - ' + user.street,
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      );
 }
