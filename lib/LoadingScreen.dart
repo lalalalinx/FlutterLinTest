a5914276c68ed8740a58324cffdb489f615e9bcb
lalalalinx <kullanart.01@gmail.com>
2021-11-09 01:53:34 +0700

    HomeView กดได้แต่ส่งไม่ได้

---
+++ lib/LoadingScreen.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: file_names, prefer_const_constructors, unused_local_variable, prefer_const_literals_to_create_immutables, curly_braces_in_flow_control_structures, dead_code, unused_import
+
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+class LoadScreen extends StatelessWidget {
+  const LoadScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/Screens/HomeView.dart
+++ lib/Screens/HomeView.dart
@@ -1,13 +1,17 @@
-// ignore_for_file: file_names, prefer_const_literals_to_create_immutables, curly_braces_in_flow_control_structures, sized_box_for_whitespace, prefer_const_constructors, duplicate_ignore
+// ignore_for_file: file_names, prefer_const_literals_to_create_immutables, curly_braces_in_flow_control_structures, sized_box_for_whitespace, prefer_const_constructors, duplicate_ignore, void_checks
 
 import 'dart:convert';
 
 import 'package:chatki_project/JSONtoDART/HomeJson.dart';
+import 'package:chatki_project/Login_Register/login.dart';
+import 'package:chatki_project/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:chatki_project/Model/HomeViewData.dart';
+import 'others/Otherprofile.dart';
+import 'package:chatki_project/settings_page.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -91,41 +95,55 @@ class _HomeViewState extends State<HomeView> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.only(
-                                top: 20, bottom: 10, left: 20, right: 20),
-                            child: Column(
+                                top: 20, bottom: 10, left: 10, right: 10),
+                            child: Row(
                               children: [
-                                TextFormField(
-                                  decoration: const InputDecoration(
-                                    //labelText: 'Search',
-                                    hintText: 'Search',
-                                    border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(30.0))),
-                                    contentPadding: EdgeInsets.symmetric(
-                                        vertical: 15, horizontal: 15),
+                                Container(
+                                  width: 312,
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      //labelText: 'Search',
+                                      hintText: 'Search by Username',
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(30),
+                                          topRight: Radius.circular(0),
+                                          bottomLeft: Radius.circular(30),
+                                          bottomRight: Radius.circular(0),
+                                        ),
+                                      ),
+                                      contentPadding: EdgeInsets.symmetric(
+                                          vertical: 15, horizontal: 15),
+                                    ),
+                                    //controller: r,
                                   ),
-                                  //controller: r,
                                 ),
-                                ElevatedButton(
-                                    onPressed: () {
-                                      // <----------------------------
-                                    },
-                                    child: const Text(
-                                      'Login',
-                                      style: TextStyle(
-                                        fontSize: 18,
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 0),
+                                  child: Container(
+                                    width: 60,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        // <----------------------------
+                                      },
+                                      child: Icon(Icons.search),
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.grey[900],
+                                        fixedSize: const Size(350, 50),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(0),
+                                              topRight: Radius.circular(30),
+                                              bottomLeft: Radius.circular(0),
+                                              bottomRight: Radius.circular(30),
+                                            ),
+                                            side: BorderSide(
+                                                color: Colors.black,
+                                                width: 1.5)),
                                       ),
                                     ),
-                                    style: ElevatedButton.styleFrom(
-                                      primary: Colors.black,
-                                      fixedSize: const Size(350, 50),
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(50),
-                                          side: BorderSide(
-                                              color: Colors.black, width: 1.5)),
-                                    ),
                                   ),
+                                ),
                               ],
                             ),
                           ),
@@ -140,39 +158,53 @@ class _HomeViewState extends State<HomeView> {
                               return Column(
                                 children: [
                                   Card(
-                                    //padding: EdgeInsets.only(left: 10,right: 10),
-                                    margin: EdgeInsets.all(10),
-                                    child: Padding(
-                                      padding:
-                                          EdgeInsets.only(top: 10, bottom: 10),
-                                      child: ListTile(
-                                        title: Row(
-                                          children: [
-                                            Container(
-                                              width: 50,
-                                              height: 50,
-                                              decoration: BoxDecoration(
-                                                color: Colors.grey[600],
-                                                borderRadius:
-                                                    BorderRadius.circular(30),
+                                    color: Colors.white,
+                                      margin: EdgeInsets.all(10),
+                                      child: Column(
+                                        children: [
+                                          //padding: EdgeInsets.only(left: 10,right: 10),
+                                          InkWell(
+                                            onTap: () {
+                                              print("tapped");
+                                            },
+                                            child: 
+                                          Padding(
+                                            padding: EdgeInsets.only(
+                                                top: 10, bottom: 10),
+                                            child: ListTile(
+                                              title: Row(
+                                                children: [
+                                                  Container(
+                                                    width: 50,
+                                                    height: 50,
+                                                    decoration: BoxDecoration(
+                                                      color: Colors.grey[600],
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              30),
+                                                    ),
+                                                  ),
+                                                  SizedBox(
+                                                    width: 20,
+                                                  ),
+                                                  Text(
+                                                      snapshot.data!.user[i]
+                                                          .userName,
+                                                      style: TextStyle(
+                                                          fontSize: 18)),
+                                                ],
+                                              ),
+                                              trailing: Text(
+                                                snapshot
+                                                    .data!.user[i].employeeId,
+                                                style: TextStyle(
+                                                    color: Colors.grey[600]),
                                               ),
                                             ),
-                                            SizedBox(
-                                              width: 20,
-                                            ),
-                                            Text(
-                                                snapshot.data!.user[i].userName,
-                                                style: TextStyle(fontSize: 18)),
-                                          ],
-                                        ),
-                                        trailing: Text(
-                                          snapshot.data!.user[i].employeeId,
-                                          style: TextStyle(
-                                              color: Colors.grey[600]),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
+                                          ),
+                                          ),
+                                        ],
+                                      )),
                                   //Divider(thickness: 1),
                                 ],
                               );
