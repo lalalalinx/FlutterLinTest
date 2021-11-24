ecc0ed193b4fa15f74d4186568d0ee786d94202d
nightsyber25 <preem.2010@hotmail.com>
2021-11-24 20:40:19 +0700

    noti

---
+++ lib/NotificationAPI.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: file_names
+
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// class NotificationAPI {
+//   static final notification = FlutterLocalNotificationsPlugin();
+
+//   static Future notificationDetails() async {
+//     return NotificationDetails(
+//       android: AndroidNotificationDetails(channelId, channelName)
+//     );
+//   }
+
+//   static Future showNotification({
+//     int id = 0,
+//     String? title,
+//     String? body,
+//     String? payload,
+
+//   }) async => notification.show(id, title, body, await notification)
+  
+// }
