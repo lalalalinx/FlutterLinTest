ed0d57102af8ca262d53f696bb748ac4a540270d
lalalalinx <kullanart.01@gmail.com>
2021-11-26 00:03:49 +0700

    Update AddMember.dart

--- lib/Screens/createGroup/AddMember.dart
+++ lib/Screens/createGroup/AddMember.dart
@@ -99,72 +99,62 @@ class _AddMemberState extends State<AddMember> {
                   )),
                   const SizedBox(height: 15),
                   searchBar(),
-                  //
+                  SizedBox(height: 20),
                   ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: searchData.length,
-          itemBuilder: (context, i) {
-            return Column(
-              children: [
-                Card(
-                    color: Colors.white,
-                    margin: EdgeInsets.all(5),
-                    child: Column(
-                      children: [
-                        //padding: EdgeInsets.only(left: 10,right: 10),
-                        InkWell(
-                          onTap: () {
-                            // Navigator.push(context,
-                            //     MaterialPageRoute(builder: (context) {
-                            //   return OtherProfile(
-                            //       targetID: snapshot.data!.user[i].employeeId,
-                            //       chatName: snapshot.data!.user[i].userName);
-                            // }));
-                          },
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 5, bottom: 5),
-                            child: ListTile(
-                              title: Row(
-                                children: [
-                                  Container(
-                                    width: 50,
-                                    height: 50,
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey[600],
-                                      borderRadius: BorderRadius.circular(30),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text(searchData[i].userName,
-                                      style: TextStyle(fontSize: 18)),
-                                ],
-                              ),
-                              trailing: Text(
-                                searchData[i].employeeId,
-                                style: TextStyle(color: Colors.grey[600]),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-                //Divider(thickness: 1),
-              ],
-            );
-          },
-        ),
-                  ListView.builder(
-                    scrollDirection: Axis.vertical,
+                    physics: NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     itemCount: searchData.length,
                     itemBuilder: (context, i) {
                       return Column(
                         children: [
-                          Text(searchData[i].employeeId),
-                          Text(searchData[i].userName),
+                          Card(
+                              color: Colors.white,
+                              margin: EdgeInsets.all(5),
+                              child: Column(
+                                children: [
+                                  //padding: EdgeInsets.only(left: 10,right: 10),
+                                  InkWell(
+                                    onTap: () {
+                                      // Navigator.push(context,
+                                      //     MaterialPageRoute(builder: (context) {
+                                      //   return OtherProfile(
+                                      //       targetID: snapshot.data!.user[i].employeeId,
+                                      //       chatName: snapshot.data!.user[i].userName);
+                                      // }));
+                                    },
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 5, bottom: 5),
+                                      child: ListTile(
+                                        title: Row(
+                                          children: [
+                                            Container(
+                                              width: 50,
+                                              height: 50,
+                                              decoration: BoxDecoration(
+                                                color: Colors.grey[600],
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              width: 20,
+                                            ),
+                                            Text(searchData[i].userName,
+                                                style: TextStyle(fontSize: 18)),
+                                          ],
+                                        ),
+                                        trailing: Text(
+                                          searchData[i].employeeId,
+                                          style: TextStyle(
+                                              color: Colors.grey[600]),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )),
+                          //Divider(thickness: 1),
                         ],
                       );
                     },
